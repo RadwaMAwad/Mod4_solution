a397653c21d0a8bea71bb5be11c039dc465441b9
RadwaMAwad <engineer.radwa20@gmail.com>
2017-01-04 11:23:16 +0200

    Add files via upload

---
+++ java module/js/SpeakGoodbye.js
@@ -0,0 +1,10 @@
+
+(function(){
+	var byeSpeaker = {};
+ byeSpeaker.speak = "John","Jen","Jason","jim";
+var speakWord = "Good Bye";
+byeSpeaker.SayHi = function(){
+	console.log(speakWord+ byeSpeaker.name);
+}
+
+})();
---
+++ java module/js/SpeakHello.js
@@ -0,0 +1,12 @@
+
+(function(){
+	var helloSpeaker = {};
+   helloSpeaker.speak= "Yaakov","Paul","Frank","Larry","Paula","Laura";
+var speakWord = "Hello";
+helloSpeaker.Sayhello = function(){
+console.log(speakWord+ helloSpeaker.name);
+
+}
+
+})();
+
